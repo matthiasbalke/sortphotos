3661b214008bd6bedd65283b3fbafdf9a6d896b6
Matthias Balke <matthias.balke@googlemail.com>
2025-06-26 16:01:13 +0200

    add scripts to test locally (#2)

---
+++ dev-run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --name sortphotos-integrationtest --rm -it matthiasbalke/sortphotos-integrationtest:local $@
---
+++ test-sorting.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# use latest official build image from branch main
+#IMAGE="ghcr.io/matthiasbalke/sortphotos-integrationtest:master"
+# use local build image
+IMAGE="matthiasbalke/sortphotos-integrationtest:local"
+
+echo ">> resetting test bed..."
+rm -rf test/target/* || true
+echo "done."
+echo ""
+
+echo ">> updating docker image..."
+docker pull ${IMAGE}
+echo "done."
+echo ""
+
+echo ">> sorting images..."
+docker run \
+  --mount type=bind,src=$(pwd)/test/source,dst=/source \
+  --mount type=bind,src=$(pwd)/test/target,dst=/target \
+  --rm -it ${IMAGE} \
+  --recursive --copy /source /target
+echo "done."
+echo ""
+
+echo ">> listing target..."
+ls -alR --color test/target
+echo "done."
